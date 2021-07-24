4631aad67a7d5e66e2595eae1825fec6b7810df8
Max Otuteye <37118417+maxotuteye@users.noreply.github.com>
2021-07-24 19:13:12 +0000

    Many bugs fixed. Closer to production

--- Base Calculator/Algorithm.cs
+++ Base Calculator/Algorithm.cs
@@ -12,20 +12,18 @@ namespace Base_Calculator
         public static string ConvertMantissa(double value, int newBase)
         {
             string output = "";
-            Stack<Int64> remainderStack = new Stack<Int64>();
+            Stack<UInt64> remainderStack = new Stack<UInt64>();
             double v = value;
 
-            while(v > 0.0 && remainderStack.Count < 20)
+            while (v > 0.0 && remainderStack.Count < 20)
             {
-                Console.WriteLine("v "+v);
-                remainderStack.Push((int)(v * (double)newBase));
-                Console.WriteLine("peek " + Convert.ToInt64(v * newBase));
-                v = Math.Abs( (double)((int)(v*newBase) - (v*newBase)));
+                remainderStack.Push((UInt64)(v * (double)newBase));
+                v = Math.Abs((double)((int)(v * newBase) - (v * newBase)));
             }
 
-            while(remainderStack.Count > 0)
+            while (remainderStack.Count > 0)
             {
-                if(remainderStack.Peek() > 9)
+                if (remainderStack.Peek() > 9)
                 {
                     char numToChar = IntToChar((int)remainderStack.Pop());
                     output += numToChar;
@@ -34,27 +32,27 @@ namespace Base_Calculator
                 {
                     output += remainderStack.Pop();
                 }
-               
+
             }
             return Reverse(output);
         }
-        
-        public static string ConvertCharacteristic(int value, int newBase)
+
+        public static string ConvertCharacteristic(UInt64 value, int newBase)
         {
             string output = "";
-            Stack<Int64> remainderStack = new Stack<Int64>();
-            int v = value;
+            Stack<UInt64> remainderStack = new Stack<UInt64>();
+            UInt64 v = value;
 
-            while(v >= newBase)
+            while (v >= (UInt64)newBase)
             {
-                remainderStack.Push(v % newBase);
-                v /= newBase;
+                remainderStack.Push(v % (UInt64)newBase);
+                v /= (UInt64)newBase;
             }
             remainderStack.Push(v);
 
-            while(remainderStack.Count > 0)
+            while (remainderStack.Count > 0)
             {
-                if(remainderStack.Peek() > 9)
+                if (remainderStack.Peek() > 9)
                 {
                     char numToChar = IntToChar((int)remainderStack.Pop());
                     output += numToChar;
@@ -69,24 +67,25 @@ namespace Base_Calculator
         }
         public static string ConvertToBase10(string value, int oldBase)
         {
+
             string characteristic = value.Split('.')[0].ToUpper();
             string mantissa = value.Split('.')[1].ToUpper();
-            long base10whole = 0;
+            UInt64 base10whole = 0;
             double base10frac = 0.0;
             List<char> wholeNum = new List<char>();
             List<char> fracNum = new List<char>();
 
             foreach (char c in characteristic)
             {
-                if(IsDigit(c))
+                if (IsDigit(c))
                 {
-                    base10whole = (oldBase * base10whole) 
-                        + Int64.Parse(c.ToString());
+                    base10whole = ((UInt64)oldBase * base10whole)
+                        + UInt64.Parse(c.ToString());
                 }
                 else
                 {
-                    base10whole = (oldBase * base10whole)
-                        + Int64.Parse(CharToInt(c).ToString());
+                    base10whole = ((UInt64)oldBase * base10whole)
+                        + UInt64.Parse(CharToInt(c).ToString());
                 }
             }
 
@@ -105,22 +104,35 @@ namespace Base_Calculator
                         Math.Pow(oldBase, -Math.Abs(i + 1));
                 }
             }
-            return ValidateInput( Convert.ToString(Convert.ToDouble(base10whole) + base10frac));
+            return ValidateInput(Convert.ToString(Convert.ToDouble(base10whole) + base10frac));
         }
         public static void Calculate(string value, int oldBase, int newBase, ref TextBox outputbox)
         {
+
             string output = "";
-            string[] newVal = 
-                ConvertToBase10(ValidateInput(value.ToUpper()), oldBase)
+            string temp = ValidateInput(value.ToUpper());
+            if (IsNegative(temp))
+            {
+                output = "-";
+                temp = temp.Remove(0, 1);
+            }
+            string[] newVal =
+                ConvertToBase10(temp, oldBase)
                 .Split('.');
-            int characteristic = (int)UInt64.Parse(newVal[0]);
-            double mantissa = Double.Parse("0."+newVal[1]);
-            output = ValidateInput ($"{ConvertCharacteristic(characteristic, newBase)}.{ConvertMantissa(mantissa, newBase)}");
-            
+            UInt64 characteristic = UInt64.Parse(newVal[0]);
+            double mantissa = Double.Parse("0." + newVal[1]);
+            output += ValidateInput($"{ConvertCharacteristic(characteristic, newBase)}.{ConvertMantissa(mantissa, newBase)}");
+
             MainWindow.UpdateOutput(output, ref outputbox);
         }
 
-         public static bool IsDigit(char c)
+        public static bool IsNegative(string value)
+        {
+            if (value.StartsWith("-")) return true;
+            else return false;
+        }
+
+        public static bool IsDigit(char c)
         {
             if (c < 91 && c > 64) return false;
             else return true;
@@ -134,7 +146,8 @@ namespace Base_Calculator
         {
             return (char)(i + 55);
         }
-        public static String ValidateInput(string value)
+
+        public static string ValidateInput(string value)
         {
             if (value.Contains('.'))
             {
@@ -142,9 +155,9 @@ namespace Base_Calculator
                     return value;
                 else return value + "0";
             }
-            
-                return value + ".0";
-           
+
+            return value + ".0";
+
         }
         public static string Reverse(string s)
         {
--- Base Calculator/MainWindow.xaml.cs
+++ Base Calculator/MainWindow.xaml.cs
@@ -17,29 +17,32 @@ using System.Text.RegularExpressions;
 
 namespace Base_Calculator
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		private DispatcherTimer timer = new DispatcherTimer();
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer = new DispatcherTimer();
+        private string lastValidText;
 
-		private bool isNumberInvalid(string text)
+        public MainWindow()
         {
-			Regex regex = new Regex("[^0-9]+");
-			return regex.IsMatch(text);
+            InitializeComponent();
         }
 
-		private void CalcBtn_Click(object sender, RoutedEventArgs e)
-		{
-			UInt64 buffer;
-			string value = inputTextBox.Text;
-			Algorithm.Calculate(value, (int)slider.Value, (int)sliderNew.Value, ref outputTextBox);
-			/*if (*//*isNumberInvalid(inputTextBox.Text) ||*//* inputTextBox.Text == "")
+        private bool isNumberInvalid(string text)
+        {
+            Regex regex = new Regex("[^-0-9A-Q]+");
+            if (regex.IsMatch(text) || text.Count(t => (t == '-')) > 1)
+                return true;
+            else return false;
+        }
+
+        private void CalcBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string value = inputTextBox.Text;
+            Algorithm.Calculate(value, (int)slider.Value, (int)sliderNew.Value, ref outputTextBox);
+            /*if (*//*isNumberInvalid(inputTextBox.Text) ||*//* inputTextBox.Text == "")
 			{
 				timer.Tick += delegate { SnackbarMessage_ActionClick(sender, e); };
 				timer.Interval = new TimeSpan(0, 0, 3);
@@ -59,52 +62,93 @@ namespace Base_Calculator
 			{
 				Algorithm.Calculate(inputTextBox.Text, slider.Value, ref outputTextBox);
 			}*/
-		}
+        }
 
-		public static void UpdateOutput(string s, ref TextBox outputbox)
-		{
-			outputbox.Text = s;
-		}
+        public static void UpdateOutput(string s, ref TextBox outputbox)
+        {
+            outputbox.Text = s;
+        }
 
-		private void timer_Tick(object sender, EventArgs e)
-		{
-			alertSnackBar.IsActive = false;
-		}
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            alertSnackBar.IsActive = false;
+        }
 
-		private void SnackbarMessage_ActionClick(object sender, RoutedEventArgs e)
-		{
-			alertSnackBar.IsActive = false;
-			timer.Stop();
-		}
+        private void SnackbarMessage_ActionClick(object sender, RoutedEventArgs e)
+        {
+            alertSnackBar.IsActive = false;
+            timer.Stop();
+        }
 
-		private void ToggleButton_Checked(object sender, RoutedEventArgs e)
-		{
-			AppBar.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
-			MainCard.Background = (SolidColorBrush)Application.Current.Resources["PrimaryCardNight"];
-			mainGrid.Background = (SolidColorBrush)Application.Current.Resources["PrimaryLightNight"];
-			inputTextBox.BorderBrush = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
-			outputTextBox.BorderBrush = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
-			slider.Foreground = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
-			slidervalbtn.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
-			sliderNew.Foreground = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
-			slidervalbtnNew.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
-			CalcBtn.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
-		}
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            AppBar.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
+            MainCard.Background = (SolidColorBrush)Application.Current.Resources["PrimaryCardNight"];
+            mainGrid.Background = (SolidColorBrush)Application.Current.Resources["PrimaryLightNight"];
+            inputTextBox.BorderBrush = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
+            outputTextBox.BorderBrush = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
+            slider.Foreground = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
+            slidervalbtn.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
+            sliderNew.Foreground = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
+            slidervalbtnNew.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
+            CalcBtn.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkNight"];
+        }
 
-		private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
-		{
-			AppBar.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
-			MainCard.Background = (SolidColorBrush)Application.Current.Resources["DayBackground"];
-			mainGrid.Background = (LinearGradientBrush)Application.Current.Resources["PrimaryLightDay"];
-			inputTextBox.BorderBrush = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
-			outputTextBox.BorderBrush = (SolidColorBrush)Application.Current.Resources["SecondaryAccent"];
-			slider.Foreground = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
-			slidervalbtn.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
-			sliderNew.Foreground = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
-			slidervalbtnNew.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
-			CalcBtn.Background = (SolidColorBrush)Application.Current.Resources["SecondaryAccent"];
-		}
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            AppBar.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
+            MainCard.Background = (SolidColorBrush)Application.Current.Resources["DayBackground"];
+            mainGrid.Background = (LinearGradientBrush)Application.Current.Resources["PrimaryLightDay"];
+            inputTextBox.BorderBrush = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
+            outputTextBox.BorderBrush = (SolidColorBrush)Application.Current.Resources["SecondaryAccent"];
+            slider.Foreground = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
+            slidervalbtn.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
+            sliderNew.Foreground = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
+            slidervalbtnNew.Background = (SolidColorBrush)Application.Current.Resources["PrimaryDarkDay"];
+            CalcBtn.Background = (SolidColorBrush)Application.Current.Resources["SecondaryAccent"];
+        }
 
-   
+        private void inputTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string currentText = inputTextBox.Text;
+            int nBase = 2;
+            if (slider.Value > nBase && currentText.Length > 0) nBase = (int)slider.Value;
+            if (!isNumberInvalid(currentText))
+            {
+                lastValidText = currentText;
+                foreach (char c in currentText)
+                {
+                    if (Algorithm.IsDigit(c) && c != '-')
+                    {
+                        if (Convert.ToInt32(Convert.ToString(c)) + 1 > nBase)
+                            nBase = Convert.ToInt32(Convert.ToString(c)) + 1;
+                    }
+                    else
+                    {
+                        if (Algorithm.CharToInt(c) + 1 > nBase)
+                            nBase = Algorithm.CharToInt(c) + 1;
+                    }
+
+                }
+            }
+            else
+            {
+                /*timer.Tick += delegate { SnackbarMessage_ActionClick(sender, e); };
+                timer.Interval = new TimeSpan(0, 0, 3);
+                timer.Start();
+                alertMsg.Content = "Invalid Input";
+                alertSnackBar.IsActive = true;*/
+                inputTextBox.Text = lastValidText;
+                inputTextBox.CaretIndex = inputTextBox.Text.Length;
+            }
+
+            slider.Value = nBase;
+            //slider.Minimum = nBase;
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            inputTextBox_TextChanged(sender, null);
+        }
     }
 }
